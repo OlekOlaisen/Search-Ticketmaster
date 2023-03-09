84e32c80beb8d4e9099c5ba823c954b2abe51531
olekristian1993 <olekristianolaisen@gmail.com>
2023-03-09 20:55:39 +0100

    misc styling

--- js/modules/search.js
+++ js/modules/search.js
@@ -5,53 +5,7 @@ export default function Search() {
   const resultsContainer = document.querySelector('#results-container');
   const apiKey = '3AHIueOLGj4rurjN2j5YRIF5Pqvmi51H';
 
-  const createResultItem = (event) => {
-    const name = event.name;
-    const dateStr = event.dates.start.localDate;
-    const dateArr = dateStr.split('-');
-    const date = `${dateArr[2]}.${dateArr[1]}.${dateArr[0]}`;
-    const venue = event._embedded.venues[0].name;
-    const imageUrl = event.images.find((image) => image.width > 500)?.url;
-    const ticketUrl = event.url;
-    const availableTickets = event.dates.status.code === 'onsale' ? 'Available tickets!' : 'Tickets not on sale';
-    const ticketStatusClass = event.dates.status.code === 'onsale' ? 'green-text' : 'red-text';
-    const category = event.classifications?.[0]?.segment.name || '';
-
-    // Renders events in HTML
-    const resultItem = document.createElement('div');
-    resultItem.classList.add('result-item');
-    resultItem.innerHTML = `
-    <a class="result-details__id" href="event.html?id=${event.id}">
-      <div class="result-image">
-        <img src="${imageUrl}" alt="${name}">
-      </div>
-      <h2 class="result-details__name">${name}</h2>
-      <div class="result-details">
-        <p class="result-details__date"><b class="event-details--bold">When:</b> ${date}</p>
-        <p class="result-details__venue"><b class="event-details--bold">Venue:</b> ${venue}</p>
-        <p class="result-details__category"><b class="event-details--bold">Category:</b> ${category}</p>
-        <p class="result-details__tickets"><b class="event-details--bold-tickets ${ticketStatusClass}">${availableTickets}</b></p>
-        <a class="result-details__button" href="${ticketUrl}" target="_blank">Buy Tickets</a>
-      </div>
-    </a>
-  `;
-
-    return resultItem;
-  };
-
-  // Loads events from sessionStorage
-  const savedEvents = sessionStorage.getItem('events');
-  let events = [];
-
-  if (savedEvents) {
-    events = JSON.parse(savedEvents);
-
-    // Renders saved events
-    events.forEach((event) => {
-      const resultItem = createResultItem(event);
-      resultsContainer.appendChild(resultItem);
-    });
-  }
+  
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
@@ -82,4 +36,53 @@ export default function Search() {
   };
 
   searchForm.addEventListener('submit', handleSearchSubmit);
+
+
+  const createResultItem = (event) => {
+    const name = event.name;
+    const dateStr = event.dates.start.localDate;
+    const dateArr = dateStr.split('-');
+    const date = `${dateArr[2]}.${dateArr[1]}.${dateArr[0]}`;
+    const venue = event._embedded.venues[0].name;
+    const imageUrl = event.images.find((image) => image.width > 500)?.url;
+    const ticketUrl = event.url;
+    const availableTickets = event.dates.status.code === 'onsale' ? 'Available tickets!' : 'Tickets not on sale';
+    const ticketStatusClass = event.dates.status.code === 'onsale' ? 'green-text' : 'red-text';
+    const category = event.classifications?.[0]?.segment.name || '';
+
+    // Renders events in HTML
+    const resultItem = document.createElement('div');
+    resultItem.classList.add('result-item');
+    resultItem.innerHTML = `
+    <a class="result-details__id" href="event.html?id=${event.id}">
+      <div class="result-image">
+        <img src="${imageUrl}" alt="${name}">
+      </div>
+      <h2 class="result-details__name">${name}</h2>
+      <div class="result-details">
+        <p class="result-details__date"><b class="event-details--bold">When:</b> ${date}</p>
+        <p class="result-details__venue"><b class="event-details--bold">Venue:</b> ${venue}</p>
+        <p class="result-details__category"><b class="event-details--bold">Category:</b> ${category}</p>
+        <p class="result-details__tickets"><b class="event-details--bold ${ticketStatusClass}">${availableTickets}</b></p>
+        <a class="result-details__button" href="${ticketUrl}" target="_blank">Buy Tickets</a>
+      </div>
+    </a>
+  `;
+
+    return resultItem;
+  };
+
+  // Loads events from sessionStorage
+  const savedEvents = sessionStorage.getItem('events');
+  let events = [];
+
+  if (savedEvents) {
+    events = JSON.parse(savedEvents);
+
+    // Renders saved events
+    events.forEach((event) => {
+      const resultItem = createResultItem(event);
+      resultsContainer.appendChild(resultItem);
+    });
+  }
 }
